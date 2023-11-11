e97f4d547af0f305fefac715033a37721ecc7835
SerefTahmaz <58826666+SerefTahmaz@users.noreply.github.com>
2023-11-11 17:22:59 +0300

    [Updated] Lobby UIs as menu item

--- Arena-Game/Assets/Scripts/UI/MainMenu/cMenuNode.cs
+++ Arena-Game/Assets/Scripts/UI/MainMenu/cMenuNode.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using DemoBlast.UI;
@@ -8,21 +9,30 @@ public class cMenuNode : MonoBehaviour
 {
     [SerializeField] private cMenuNode m_ParentNode;
     [SerializeField] private cView m_ChildsView;
+    [SerializeField] private UnityEvent m_OnActivateEvent;
+    [SerializeField] private UnityEvent m_OnDeActivateEvent;
+
+    public void SetParentNode(cMenuNode menuNode)
+    {
+        m_ParentNode = menuNode;
+    }
 
     public void Activate()
     {
         if(m_ParentNode != null) m_ParentNode.Deactivate();
         if(m_ChildsView != null) m_ChildsView.Activate();
+        m_OnActivateEvent.Invoke();
     }
     
     public void Deactivate()
     {
         if(m_ChildsView != null) m_ChildsView.Deactivate();
+        m_OnDeActivateEvent.Invoke();
     }
     
     public void EnableParent()
     {
         if(m_ParentNode != null) m_ParentNode.Activate();
-        if(m_ChildsView != null) m_ChildsView.Deactivate();
+        Deactivate();
     }
 }
