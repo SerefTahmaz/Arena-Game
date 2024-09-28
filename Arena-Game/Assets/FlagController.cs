5a39854d5160e078b30a472a758f4ee8f77f63f8
SerefTahmaz <sereftahmaz@outlook.com>
2024-09-28 03:35:14 +0300

    [Added] Flags to snow level

---
+++ Arena-Game/Assets/FlagController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagController : MonoBehaviour
+{
+    [SerializeField] private Animator m_Animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator.SetFloat("RandomOffset", Random.Range(0.0f,1.0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
