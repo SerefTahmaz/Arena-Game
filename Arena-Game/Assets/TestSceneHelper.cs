1e1fb9722bb2cd36e6ced2af406a0a5c52c0b8b6
SerefTahmaz <sereftahmaz@outlook.com>
2024-11-05 04:25:08 +0300

    [Updated] Scripts paths

--- Arena-Game/Assets/TestSceneHelper.cs
+++ Arena-Game/Assets/TestSceneHelper.cs
@@ -2,22 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using Gameplay.Character;
 using Unity.Netcode;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class TestSceneHelper : MonoBehaviour
 {
-    [SerializeField] private bool m_SpawnPlayerOnStart;
-    [SerializeField] private SceneAsset m_DebugLevel;
+#if UNITY_EDITOR
+   [SerializeField] private bool m_SpawnPlayerOnStart;
+   [SerializeField] private SceneAsset m_DebugLevel;
     
       
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (m_SpawnPlayerOnStart)
-        {
-            SceneManager.LoadScene(m_DebugLevel.name,LoadSceneMode.Additive);
-        }
-    }
+   // Start is called before the first frame update
+   void Start()
+   {
+      if (m_SpawnPlayerOnStart)
+      {
+         SceneManager.LoadScene(m_DebugLevel.name,LoadSceneMode.Additive);
+      }
+   }
+#endif
 }
