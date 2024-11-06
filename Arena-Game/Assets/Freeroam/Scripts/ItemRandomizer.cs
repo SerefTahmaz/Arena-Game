eed0716c0e414d4f9aef067f1a8d85e0a87507b4
SerefTahmaz <sereftahmaz@outlook.com>
2024-11-06 20:12:20 +0300

    [Added] Female 2

--- Arena-Game/Assets/Freeroam/Scripts/ItemRandomizer.cs
+++ Arena-Game/Assets/Freeroam/Scripts/ItemRandomizer.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using ArenaGame.Utils;
 using UnityEngine;
 using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
@@ -20,6 +22,7 @@ public class ItemRandomizer : MonoBehaviour
     {
         public List<ItemWrapper> m_Items;
         public bool m_IsExclusive;
+        public bool m_IsOneHasToBeSeleceted;
     }
     
     
@@ -36,16 +39,25 @@ public class ItemRandomizer : MonoBehaviour
 
         foreach (var itemGroup in m_ItemGroups)
         {
+            bool oneSelected = false;
+           
             foreach (var item in itemGroup.m_Items)
             {
                 var value = Random.value;
                 if (item.m_Possibilty >= value)
                 {
+                    oneSelected = true;
                     Debug.Log(value);
                     item.m_Item.SetActive(true);
                     if(itemGroup.m_IsExclusive) break;
                 }
             }
+
+            if (itemGroup.m_IsOneHasToBeSeleceted && !oneSelected)
+            {
+                itemGroup.m_Items.RandomItem().m_Item.SetActive(true);
+            }
+            
         }
     }
 }
