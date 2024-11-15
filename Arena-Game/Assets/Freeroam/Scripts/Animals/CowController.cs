b88561bd78a110a98f9017e7c3e9919126b34232
SerefTahmaz <sereftahmaz@outlook.com>
2024-11-16 00:02:04 +0300

    Update CowController.cs

--- Arena-Game/Assets/Freeroam/Scripts/Animals/CowController.cs
+++ Arena-Game/Assets/Freeroam/Scripts/Animals/CowController.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using ArenaGame.Utils;
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CowController : MonoBehaviour
 {
@@ -23,7 +25,7 @@ public class CowController : MonoBehaviour
     {
         while (true)
         {
-            await UniTask.WaitForSeconds(Random.Range(m_PickingPointDelay.x, m_PickingPointDelay.y));
+            await UniTask.WaitForSeconds(Random.Range(m_PickingPointDelay.x, m_PickingPointDelay.y), cancellationToken: this.GetCancellationTokenOnDestroy());
             await PickAPoint();
         }
     }
@@ -37,20 +39,21 @@ public class CowController : MonoBehaviour
 
         List<UniTask> tasks = new List<UniTask>();
         
-        var rotT = transform.DORotateQuaternion(Quaternion.LookRotation(dir), 1).ToUniTask();
-        var moveT = transform.DOMove(point, m_Speed).SetSpeedBased().SetEase(Ease.Linear).SetUpdate(UpdateType.Fixed).ToUniTask();
+        var rotT = transform.DORotateQuaternion(Quaternion.LookRotation(dir), 1).ToUniTask(cancellationToken:this.GetCancellationTokenOnDestroy());
+        var moveT = transform.DOMove(point, m_Speed).SetSpeedBased().SetEase(Ease.Linear).SetUpdate(UpdateType.Fixed).ToUniTask(cancellationToken:this.GetCancellationTokenOnDestroy());
         var animT = DOVirtual.Float(0, 1, 0.25f, value =>
         {
             m_CowAnimator.SetFloat("Forward",value);
-        }).SetEase(Ease.Linear).ToUniTask();
+        }).SetEase(Ease.Linear).ToUniTask(cancellationToken:this.GetCancellationTokenOnDestroy());
         
         tasks.Add(rotT);
         tasks.Add(moveT);
         tasks.Add(animT);
         await UniTask.WhenAll(tasks);
+        await UniTask.WaitForSeconds(0.01f, cancellationToken: this.GetCancellationTokenOnDestroy());
         await DOVirtual.Float(1, 0, 0.25f, value =>
         {
             m_CowAnimator.SetFloat("Forward",value);
-        }).SetEase(Ease.Linear).ToUniTask();
+        }).SetEase(Ease.Linear).ToUniTask(cancellationToken: this.GetCancellationTokenOnDestroy());
     }
 }
