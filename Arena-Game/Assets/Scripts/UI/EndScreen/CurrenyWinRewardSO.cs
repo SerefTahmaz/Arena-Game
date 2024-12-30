e14beecae11f3d04b01a7674490217ac01f6ce34
SerefTahmaz <sereftahmaz@outlook.com>
2024-12-30 20:29:47 +0300

    [Fixed] Give current map reward currency

--- Arena-Game/Assets/Scripts/UI/EndScreen/CurrenyWinRewardSO.cs
+++ Arena-Game/Assets/Scripts/UI/EndScreen/CurrenyWinRewardSO.cs
@@ -6,13 +6,13 @@ namespace DefaultNamespace
     [CreateAssetMenu(fileName = "MoneyWinReward", menuName = "Game/Rewards/CurrencyWinReward", order = 0)]
     public class CurrenyWinRewardSO : BaseWinRewardSO
     {
-        [SerializeField] private int m_Amount;
-
+        // [SerializeField] private int m_Amount;
+ 
         public override IWinReward CreateRewardIns()
         {
             base.CreateRewardIns();
             var ins = Instantiate(PrefabList.Get().CurrencyRewardPrefab);
-            ins.Init(m_Amount);
+            ins.Init(MapManager.Instance.GetCurrentMap().RewardCurrency);
             return ins;
         }
     }
