6f1e4492253a280ee154b87c8a7434fd17090313
SerefTahmaz <sereftahmaz@outlook.com>
2024-12-31 23:02:35 +0300

    [Added] ATT and privacy policy

---
+++ Arena-Game/Assets/Samples/iOS 14 Advertising Support/1.0.0/01 Context Screen/Scripts/ContextScreenManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using Unity.Advertisement.IosSupport.Components;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Unity.Advertisement.IosSupport.Samples
+{
+    /// <summary>
+    /// This component will trigger the context screen to appear when the scene starts,
+    /// if the user hasn't already responded to the iOS tracking dialog.
+    /// </summary>
+    public class ContextScreenManager : MonoBehaviour
+    {
+        void Start()
+        {
+#if UNITY_IOS
+            // check with iOS to see if the user has accepted or declined tracking
+            var status = ATTrackingStatusBinding.GetAuthorizationTrackingStatus();
+
+            if (status == ATTrackingStatusBinding.AuthorizationTrackingStatus.NOT_DETERMINED)
+            {
+                Debug.Log("Unity iOS Support: Requesting iOS App Tracking Transparency native dialog.");
+
+                ATTrackingStatusBinding.RequestAuthorizationTracking();
+            }
+#else
+            Debug.Log("Unity iOS Support: App Tracking Transparency status not checked, because the platform is not iOS.");
+#endif
+            LoadScene();
+        }
+
+        private async UniTask LoadScene()
+        {
+#if UNITY_IOS && !UNITY_EDITOR
+            await UniTask.WaitWhile((() =>
+                ATTrackingStatusBinding.GetAuthorizationTrackingStatus() ==
+                ATTrackingStatusBinding.AuthorizationTrackingStatus.NOT_DETERMINED));
+#endif
+            SceneManager.LoadScene(1);
+        }
+    }   
+} 
