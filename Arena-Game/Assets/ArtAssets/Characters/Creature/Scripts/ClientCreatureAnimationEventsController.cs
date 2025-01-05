8b58a83ecdb075640a3781058bc1444f8939461d
SerefTahmaz <sereftahmaz@outlook.com>
2025-01-05 13:11:04 +0300

    Update ClientCreatureAnimationEventsController.cs

--- Arena-Game/Assets/ArtAssets/Characters/Creature/Scripts/ClientCreatureAnimationEventsController.cs
+++ Arena-Game/Assets/ArtAssets/Characters/Creature/Scripts/ClientCreatureAnimationEventsController.cs
@@ -40,90 +40,105 @@ namespace ArtAssets.Characters.Creature.Scripts
         [ClientRpc]
         private void OnRolImpactClientRpc()
         {
+            if (IsHost) return;
             m_CreatureAnimationEventsController.RolImpactAction();
         }
         
         [ClientRpc]
         private void OnRightSlashStartClientRpc()
         {
+            if (IsHost) return;
             m_CreatureAnimationEventsController.RightSlashStartAction();
         }
         
         [ClientRpc]
         private void OnRightSlashEndClientRpc()
         {
+            if (IsHost) return;
             m_CreatureAnimationEventsController.RightSlashEndAction();
         }
         
         [ClientRpc]
         private void OnLeftSlashStartClientRpc()
         {
+            if (IsHost) return;
             m_CreatureAnimationEventsController.LeftSlashStartAction();
         }
         
         [ClientRpc]
         private void OnBothSlashStartClientRpc()
         {
+            if (IsHost) return;
             m_CreatureAnimationEventsController.BothSlashStartAction();
         }
         
         [ClientRpc]
         private void OnBothSlashEndClientRpc()
         {
+            if (IsHost) return;
             m_CreatureAnimationEventsController.BothSlashEndAction();
         }
         
         [ClientRpc]
         private void OnLeftStepEventClientRpc()
         {
+            if (IsHost) return;
             m_CreatureAnimationEventsController.OnLeftStepAction();
         }
         
         [ClientRpc]
         private void OnRightStepEventClientRpc()
         {
+            if (IsHost) return;
             m_CreatureAnimationEventsController.OnRightStepAction();
         }
         
         [ClientRpc]
         private void OnClawSlashStartEventClientRpc()
         {
+            if (IsHost) return;
             m_CreatureAnimationEventsController.OnClawSlashStartAction();
         }
         
         [ClientRpc]
         private void OnMutantSwipingStartEventClientRpc()
         {
+            if (IsHost) return;
             m_CreatureAnimationEventsController.OnMutantSwipingStartAction();
         }
         
         [ClientRpc]
         private void OnSpinAttackStartEventClientRpc()
         {
+            if (IsHost) return;
             m_CreatureAnimationEventsController.OnSpinAttackStartAction();
         }
         
         [ClientRpc]
         private void OnComboAttackStartEventClientRpc()
         {
+            if (IsHost) return;
             m_CreatureAnimationEventsController.OnComboAttackStartAction();
         }
         
         [ClientRpc]
         private void OnRollStartEventClientRpc()
         {
+            if (IsHost) return;
             m_CreatureAnimationEventsController.OnRollStartAction();
         }
         
         [ClientRpc]
         private void OnDeadStartClientRpc()
         {
+            if (IsHost) return;
             m_CreatureAnimationEventsController.DeadStartAction();
         }
         
         [ClientRpc]
         private void OnHitBodyStartClientRpc()
         {
+            if (IsHost) return;
             m_CreatureAnimationEventsController.HitBodyStartAction();
         }
     }
